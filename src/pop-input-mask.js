e9920eed7b133e83f56b79d6ab19c56bce813597
EdBozhko <eduard.bozhko92@gmail.com>
2021-02-26 17:44:22 +0200

    put cursor on start on telephone input fields

--- src/pop-input-mask.js
+++ src/pop-input-mask.js
@@ -1,8 +1,24 @@
-//Код jQuery, установливающий маску для ввода телефона элементу input
-        //1. После загрузки страницы,  когда все элементы будут доступны выполнить...
-        $(function(){
-            //2. Получить элемент, к которому необходимо добавить маску
-            $("#your-telephone--callback").mask("+38 (999) 999-99-99");
-            $("#your-telephone--calculation").mask("+38 (999) 999-99-99");
-            $("#your-telephone--download").mask("+38 (999) 999-99-99");
-          });
+$.fn.setCursorPosition = function(pos) {
+  if ($(this).get(0).setSelectionRange) {
+    $(this).get(0).setSelectionRange(pos, pos);
+  } else if ($(this).get(0).createTextRange) {
+    var range = $(this).get(0).createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+};
+
+$("#your-telephone--callback").click(function(){
+  $(this).setCursorPosition(5);
+}).mask("+38 (999) 999-99-99");
+
+$("#your-telephone--calculation").click(function(){
+  $(this).setCursorPosition(5);
+}).mask("+38 (999) 999-99-99");
+
+$("#your-telephone--download").click(function(){
+  $(this).setCursorPosition(5);
+}).mask("+38 (999) 999-99-99");
+
